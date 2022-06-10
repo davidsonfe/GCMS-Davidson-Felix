d88f3bd277c44d8ec7e300123beccecbb4bb161b
Davidson Felix da Silva <davidson@MacBook-Pro-de-Davidson.local>
2022-06-10 17:08:44 -0300

    commit antes do merge com hotfix da pratica04 - branch_master

--- src/br/com/ifpe/HelloWold.java
+++ src/br/com/ifpe/HelloWold.java
@@ -6,6 +6,8 @@ public class HelloWold {
         // write your code here
         System.out.println("Hello Wold!!");
 
-        System.out.println("Primeiro commit - Teste_pratica03");
+        System.out.println("Primeiro commit - Teste_pratica04 branch - hotfix");
+
+        System.out.println("Primeiro commit - Teste_pratica04 branch - master");
     }
 }
